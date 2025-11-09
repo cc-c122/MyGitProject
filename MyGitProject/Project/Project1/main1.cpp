65380cf60bce0dcc057d0ad908547d014236b707
cc-c122 <2513326504@qq.com>
2025-11-09 15:28:09 +0800

    海选问题

--- MyGitProject/Project/Project1/main1.cpp
+++ MyGitProject/Project/Project1/main1.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int x[10000000];
+int mm[10000][10000];
 
 int main() {
-	int n, m, j, o, temp=0;
+	int n = 0, m = 0, max = 0, a = 0, b = 0;
 	while (cin >> n >> m) {
-		o = 0, j = 0 ;
-		if (n > m) {
-			n = temp, temp = m, m = n;
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= m; ++j) {
+				cin >> mm[i][j];
+			}
 		}
-		for (int i = n; i <= m; ++i) {
-			x[i] = i;//输入数组内容
-			 if (x[i] % 2) {
-				 j += x[i] * x[i] * x[i];
-			 }
-			 else {
-				 o += x[i] * x[i];
-			 }
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= m; ++j) {
+				if (mm[i][j] < 0) {
+					mm[i][j] = ~mm[i][j] + 1;//
+			    }
+				if (max < mm[i][j]) {
+					max = mm[i][j];
+					a = i, b = j;
+				}
+			}
 		}
-		cout << j << ' ' << o << endl;
+		cout << a << ' ' << b << ' '<< max << endl;
 	}
 	return 0;
 }
