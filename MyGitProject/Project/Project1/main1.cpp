b91e34a6c94c0a707aef6c6334619fa909d10808
cc-c122 <2513326504@qq.com>
2025-11-10 17:27:58 +0800

    比大小

--- MyGitProject/Project/Project1/main1.cpp
+++ MyGitProject/Project/Project1/main1.cpp
@@ -1,28 +1,34 @@
-#include <iostream>
+# include <iostream>
 using namespace std;
 
-int mm[10000][10000];
+void so(int &a, int &b, int &c);
 
 int main() {
-	int n = 0, m = 0, max = 0, a = 0, b = 0;
-	while (cin >> n >> m) {
-		for (int i = 1; i <= n; ++i) {
-			for (int j = 1; j <= m; ++j) {
-				cin >> mm[i][j];
-			}
+	int t = 0;
+	cin >> t;
+	while (t--) {
+		int a = 0, b = 0, c = 0;
+		cin >> a >> b >> c;
+		so(a, b, c);
+		if (b + c > a) {
+			cout << "YES" << endl;
 		}
-		for (int i = 1; i <= n; ++i) {
-			for (int j = 1; j <= m; ++j) {
-				if (mm[i][j] < 0) {
-					mm[i][j] = ~mm[i][j] + 1;//
-			    }
-				if (max < mm[i][j]) {
-					max = mm[i][j];
-					a = i, b = j;
-				}
-			}
+		else {
+			cout << "NO" << endl;
 		}
-		cout << a << ' ' << b << ' '<< max << endl;
 	}
-	return 0;
+	return 0;//
+}
+
+void so(int &a, int &b, int &c) {
+	int tmp = 0;
+	if (a < b) {
+		tmp = a, a = b , b = tmp;
+	}
+	if (a < c) {
+		tmp = a, a = c, c = tmp;
+	}
+	if (b < c) {
+		tmp = b, b = c, c = tmp;
+	}
 }
