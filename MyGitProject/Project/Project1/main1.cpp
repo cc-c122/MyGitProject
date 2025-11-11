702cca682cdf872386f9ee415a753bbeffc0e2a9
cc-c122 <2513326504@qq.com>
2025-11-11 16:16:07 +0800

    字符指针（char*）特殊处理： 字宗从地址开始依次打印字符，直到遇到’/0‘停止

--- MyGitProject/Project/Project1/main1.cpp
+++ MyGitProject/Project/Project1/main1.cpp
@@ -1,45 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int n[100];
-int sushu(int);
-
 int main() {
-	int x = 0, y = 0;
-	while (cin >> x >> y) {
-		if (x == 0 && y == 0) {
-			break;
-		}
-		int num = 0;
-		for (int i = x; i <= y; ++i) {
-			n[i] = i * i + i + 41;
-		}
-		for (int i = x; i <= y; ++i) {
-			num += sushu(n[i]);
-		}
-		if (y - x + 1 == num) {
-			cout << "OK" << endl;//
-		}
-		else {
-			cout << "SORRY" << endl;
-		}
+	char a [] = "i";
+	char b[] = "love";
+	char c[] = "you";
+	char* p[3];
+	p[0] = a;
+	p[1] = b;
+	p[2] = c;
+	cout << p[0] << p[1] << p[2] << endl;
+	cout << *p[0] << *p[1] << *p[2] << endl;//
+	for (int i = 0; i < 3; ++i) {
+		cout << p[i] << " ";
 	}
+	cout << endl;
 	return 0;
-}
-
-int  sushu(int x) {
-	if (x < 2) {
-		return 0;
-	}
-	else if (x == 2) {
-		return 1;
-	}
-	else {
-		for (int i = 2; i * i <= x; ++i) {
-			if (x % i == 0) {
-				return 0;
-			}	
-		}
-		return 1;
-	}
 }
